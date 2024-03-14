90d2ce8fbf08fec4d1f46b58f76901a37f5f1ae0
Laila-dAM <lailamota.238@gmail.com>
2024-03-14 18:04:30 -0300

    Add a new node to the linked list, updating the last node

--- text.c
+++ text.c
@@ -9,3 +9,21 @@ typedef struct Node {
     char line[MAX_LINE_LEN];
     struct Node *next;
 } Node;
+
+Node *head = NULL;
+Node *current = NULL;
+
+void insertLine(const char *text) {
+    Node *newNode = (Node *)malloc(sizeof(Node));
+    strcpy(newNode->line, text);
+    newNode->next = NULL;
+    
+
+if (head == NULL) {
+    head = newNode;
+    current = newNode;
+} else {
+    current->next = newNode;
+    current = newNode;
+}
+}
